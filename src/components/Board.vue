fe4da43c3a15312dc1a8348ce5e5f0a4fc08b31d
DESKTOP-P0ALHGR\Youn <yjh6406@gmail.com>
2019-04-16 18:26:46 +0900

    no message

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -2,11 +2,14 @@
     <div>
         Board.vue 파일이~~~다~~        
         <div>
-            bid : {{bid}}
-            <router-link to="/b/2">Board2</router-link>
-            <router-link to="/b/3">Board3</router-link>
-            <router-link to="/b/${bid}/c/1">Card1</router-link>
-            <router-link to="/b/${bid}/c/2">Card2</router-link>
+            <div v-if="loading">Loading 중입니다...</div>
+            <div v-else>
+                bid : {{bid}}
+                <router-link to="/b/2">Board2</router-link>
+                <router-link to="/b/3">Board3</router-link>
+                <router-link to="/b/${bid}/c/1">Card1</router-link>
+                <router-link to="/b/${bid}/c/2">Card2</router-link>
+            </div>
             <hr />
             <router-view></router-view>
         </div>
@@ -17,13 +20,23 @@
 export default {
     data(){
         return {
-            bid :0
+            bid :0,
+            loading : false
         }
     },
     created(){
+        this.fetchData()
         console.log(this.$route)
         console.log(this.$route.params.bid)
-        this.bid = this.$route.params.bid
+    },
+    methods:{
+        fetchData(){
+            this.loading = true  
+            setTimeout(()=>{
+                this.bid = this.$route.params.bid
+                this.loading = false  
+            },500)          
+        }
     }
 }
 </script>
--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,7 +1,8 @@
 <template>
     <div>
         card vue~~
-        <div>cid : {{cid}}</div>
+        <div v-if="loading">loading~~card~~~</div>
+        <div v-else>cid : {{cid}}</div>
     </div>
 </template>
 
@@ -9,18 +10,33 @@
 export default {
     data(){
         return {
-            cid: 0
+            cid: 0,
+            loading : false
         }
     },
     watch:{
-        '$route'(){
-            this.cid = this.$route.params.cid
+        // '$route'(){
+        //     this.cid = this.$route.params.cid
+        //     this.fetchData()
+        // }
+        $route:{
+            handler:'fetchData',
+            immediate : true     //바로 실행하는 방법이므로 아래의 created와 중복이 된다.
         }
     },
-    created(){
-        console.log("card.vue")
-        console.log(this.$route.params)
-        this.cid = this.$route.params.cid
+    // created(){
+    //     console.log("card.vue")
+    //     console.log(this.$route.params)
+    //     this.fetchData()
+    // },
+    methods:{
+        fetchData(){
+            this.loading = true
+            setTimeout(()=>{
+                this.cid = this.$route.params.cid
+                this.loading = false
+            },500)
+        }
     }
 }
 </script>
